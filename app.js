26d3b45bdc5aba01bfbd0e9ccc6ac5e5ddc5f376
nndhuy <nndhuy@cmcglobal.vn>
2022-08-09 14:21:30 +0700

    breaking out review routes

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const express = require('express')
+const path = require('path')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
-const { campgroundSchema, reviewSchema } = require('./schemas.js')
-const path = require('path')
-const methodOverride = require('method-override')
-const Campground = require('./models/campground')
-const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
-const Joi = require('joi')
-const Review = require('./models/review')
+const methodOverride = require('method-override')
 
 const campgrounds = require('./routes/campgrounds')
+const reviews = require('./routes/reviews')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -21,57 +17,23 @@ db.once('open', function () {
 });
 
 const app = express()
-
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
-
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body)
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next()
-    }
-}
-
 app.use('/campgrounds', campgrounds)
-
-
-// Create Review
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    const review = new Review( req.body.review)
-    campground.reviews.push(review)
-    review.save()
-    campground.save()
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-    const {id, reviewId} = req.params
-    const campground = await Campground.findByIdAndUpdate(id, {$pull: {
-        reviews: reviewId
-    }})
-    await Review.findByIdAndDelete(reviewId)
-    res.redirect(`/campgrounds/${id}`)
-}))
+app.use('/campgrounds/:id/reviews', reviews)
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
 })
-
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err
     if (!err.message) err.message = 'Something went wrong'
     res.status(statusCode).render('error', { err })
 })
-
 app.listen(3000, () => {
     console.log('Serving on port 3000')
 })
