c0aecb361a4778258b62f1034db9bb3cc4361a77
nndhuy <nndhuy@cmcglobal.vn>
2022-08-29 18:38:39 +0700

    review permissions

--- seeds/index.js
+++ seeds/index.js
@@ -19,7 +19,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
-            author: '63099cd38246a5869ea28582',
+            author: '6307296a2ea05357120bcf9f',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
